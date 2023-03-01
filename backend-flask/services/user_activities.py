dd97c166208ec7508a90172119b9d913a421f506
Marika Bergman <marika.bergman@gmail.com>
2023-03-01 17:12:18 +0000

    fix indentation error

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -4,35 +4,35 @@ from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
     # x-ray -----
-      segment = xray_reorder.begin_segment('user_activities')
-      model = {
+    segment = xray_reorder.begin_segment('user_activities')
+    model = {
         'errors': None,
         'data': None
-      }
+    }
 
-      now = datetime.now(timezone.utc).astimezone()
+    now = datetime.now(timezone.utc).astimezone()
      
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        now = datetime.now()
-        results = [{
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Andrew Brown',
-          'message': 'Cloud is fun!',
-          'created_at': (now - timedelta(days=1)).isoformat(),
-          'expires_at': (now + timedelta(days=31)).isoformat()
-        }]
-        model['data'] = results
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      now = datetime.now()
+      results = [{
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'handle':  'Andrew Brown',
+        'message': 'Cloud is fun!',
+        'created_at': (now - timedelta(days=1)).isoformat(),
+        'expires_at': (now + timedelta(days=31)).isoformat()  
+      }]
+      model['data'] = results
         
        # x-ray ----
       subsegment = xray_recorder.begin_subsegment('mock-data')
       
-       dict = {
-        "now": now.isoformat()
+      dict = {
+        "now": now.isoformat(),
         "result-size": len(model['data'])
       }
       
       subsegment.put_metadata('key', dict, 'namespace')  
         
-      return model
+      return model  
