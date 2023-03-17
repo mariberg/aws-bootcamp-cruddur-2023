791775cd9e5edaccd79ca96d0313924d0f57021d
Marika Bergman <marika.bergman@gmail.com>
2023-03-17 10:40:12 +0000

    sql query works

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 import logging
 
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
@@ -14,15 +14,27 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
  
-      sql = """
-      SELECT * FROM activities
-      """
+      sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchall()
-      return json[0]
-      return results
-    
+          json = cur.fetchone()
+      print("12000")
+      return json[0]    
