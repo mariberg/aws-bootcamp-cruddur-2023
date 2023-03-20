d33857a33360ac43b2b86fcf5328e537b66b3d5d
Marika Bergman <marika.bergman@gmail.com>
2023-03-20 14:54:53 +0000

    add functionality to write messages

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,12 +1,18 @@
 import uuid
 from datetime import datetime, timedelta, timezone
+
+from lib.db import db
+
 class CreateActivity:
+  def validations():
   def run(message, user_handle, ttl):
     model = {
       'errors': None,
       'data': None
     }
 
+    user_uuid = ''
+    
     now = datetime.now(timezone.utc).astimezone()
 
     if (ttl == '30-days'):
@@ -40,6 +46,7 @@ class CreateActivity:
         'message': message
       }   
     else:
+      self.create_activity()
       model['data'] = {
         'uuid': uuid.uuid4(),
         'display_name': 'Andrew Brown',
@@ -49,3 +56,21 @@ class CreateActivity:
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
+  def create_activity(user_uuid, message, expires_at):
+    sql = f"""
+    INSERT INTO (
+      user_uuid,
+      message,
+      expires_at
+    )
+    VALUES (
+      %s,
+      %s,
+      %s
+    ) RETURNING uuid;
+    """
+    uuid = db.query_commit_returning_id(sql, user_uuid, message, expires_at)
+    
+    query_commit(sql)
+  
+  def query_object_activity():
