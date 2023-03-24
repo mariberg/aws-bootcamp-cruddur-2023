e636846e3a647a8f6e0d57221f7c95848b077306
Marika Bergman <marika.bergman@gmail.com>
2023-03-24 11:46:59 +0000

    implement scripts for DynamoDB

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,5 +1,5 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
