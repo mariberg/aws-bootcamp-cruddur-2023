a5d15e174a51cb61536a0567b4b788029227863b
Marika Bergman <marika.bergman@gmail.com>
2023-03-29 13:53:35 +0000

    continue implementing conversations

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -15,13 +15,16 @@ class Ddb:
     return dynamodb
 
   def list_message_groups(client,my_user_uuid):
+    current_year = str(datetime.now().year)
     table_name = 'cruddur-messages'
     query_params = {
       'TableName': table_name,
-      'KeyConditionExpression': 'pk = :pk',
+      'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
+      ':year': {'S': '2023'},
       'ScanIndexForward': False,
       'Limit': 20,
       'ExpressionAttributeValues': {
+        ':year': {'S': current_year },
         ':pk': {'S': f"GRP#{my_user_uuid}"}
       }
     }
@@ -42,4 +45,36 @@ class Ddb:
         'message': item['message']['S'],
         'created_at': last_sent_at
       })
+    return results
+  
+  def list_messages(client,message_group_uuid):
+    current_year = str(datetime.now().year)
+    table_name = 'cruddur-messages'
+    query_params = {
+      'TableName': table_name,
+      'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
+      'ScanIndexForward': False,
+      'Limit': 40,
+      'ExpressionAttributeValues': {
+        ':year': {'S': current_year },
+        ':pk': {'S': f"MSG#{message_group_uuid}"}
+      }
+    }
+
+    response = client.query(**query_params)
+    items = response['Items']
+    items.reverse()
+    
+    results = []
+    for item in items:
+      created_at = item['sk']['S']
+      results.append({
+        'uuid': item['message_uuid']['S'],
+        'display_name': item['user_display_name']['S'],
+        'handle': item['user_handle']['S'],
+        'message': item['message']['S'],
+        'created_at': created_at
+      })
+    print("EEE")
+    print(results)
     return results
