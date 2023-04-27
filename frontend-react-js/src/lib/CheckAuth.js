7e06c01de341667d3060167bc63afbc7c98d760d
Marika Bergman <marika.bergman@gmail.com>
2023-04-27 12:57:14 +0000

    Implement Refresh Token for Amazon Cognito

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Auth } from 'aws-amplify';
  
+export async function getAccessToken() {
+  Auth.currentSession()
+  .then((cognito_user_session) => {
+    const access_token = cognito_user_session.accessToken.jwtToken;
+    localStorage.setItem("access_token", access_token)
+  })
+.catch((err) => console.log(err));
+}
 
-
-const checkAuth = async (setUser) => {
+export async function checkAuth(setUser) {
     Auth.currentAuthenticatedUser({
       // Optional, By default is false. 
       // If set to true, this call will send a 
       // request to Cognito to get the latest user data
       bypassCache: false 
     })
-    .then((user) => {
-      console.log('user',user);
+    .then((cognito_user) => {
+      console.log('cognito_user',cognito_user);
+      setUser({
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
+      })
       return Auth.currentAuthenticatedUser()
-    }).then((cognito_user) => {
-        setUser({
-          display_name: cognito_user.attributes.name,
-          handle: cognito_user.attributes.preferred_username
-        })
+    }).then((cognito_user_session) => {
+        console.log('cognito_user_session',cognito_user_session);
+        localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
     })
     .catch((err) => console.log(err));
   };
   
 
-  export default checkAuth;
+  export {checkAuth, getAccessToken};
