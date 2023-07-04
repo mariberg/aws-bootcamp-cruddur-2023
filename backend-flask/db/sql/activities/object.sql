3e56722b435914753eecd8835cd96a91f0444057
Marika Bergman <marika.bergman@gmail.com>
2023-07-04 12:46:26 +0000

    fix the activity form not to be hardcoded

--- backend-flask/db/sql/activities/object.sql
+++ backend-flask/db/sql/activities/object.sql
@@ -1,10 +1,10 @@
 SELECT 
     activities.uuid,
-    users.diplay_name,
+    users.display_name,
     users.handle,
     activities.message,
     activities.created_at,
-    activities.expired_at
+    activities.expires_at
 FROM public.activities
 INNER JOIN public.users ON users.uuid = activities.user_uuid
 WHERE
